3f84340f9e4b3a8e33813f07d6563dd127a645b2
luccacastro <luccazutin@gmail.com>
2024-11-11 11:42:34 +0000

    redirect to login

--- routes/web.php
+++ routes/web.php
@@ -17,12 +17,7 @@ use App\Http\Controllers\TaskController;
 */
 
 Route::get('/', function () {
-    return Inertia::render('Welcome', [
-        'canLogin' => Route::has('login'),
-        'canRegister' => Route::has('register'),
-        'laravelVersion' => Application::VERSION,
-        'phpVersion' => PHP_VERSION,
-    ]);
+    return redirect()->route('login');
 });
 
 Route::middleware([
